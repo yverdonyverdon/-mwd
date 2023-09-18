dab60db187fefc69cee68590d78e097049b88611
Yverdon <yverdonb@gmail.com>
2023-09-18 11:49:20 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,27 @@
-import { Scroll, ScrollControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { MotionConfig } from "framer-motion";
-import { Leva } from "leva";
-import { useEffect, useState } from "react";
-import { Cursor } from "./components/Cursor";
-import { Experience } from "./components/Experience";
-import { Interface } from "./components/Interface";
-import { Menu } from "./components/Menu";
-import { ScrollManager } from "./components/ScrollManager";
-import { framerMotionConfig } from "./config";
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from "./components"
 
-function App() {
-  const [section, setSection] = useState(0);
-  const [menuOpened, setMenuOpened] = useState(false);
-
-  useEffect(() => {
-    setMenuOpened(false);
-  }, [section]);
+const App = () => {
 
   return (
-    <>
-      <MotionConfig
-        transition={{
-          ...framerMotionConfig,
-        }}
-      >
-        <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
-          <color attach="background" args={["#e6e7ff"]} />
-          <ScrollControls pages={4} damping={0.1}>
-            <ScrollManager section={section} onSectionChange={setSection} />
-            <Scroll>
-              <Experience section={section} menuOpened={menuOpened} />
-            </Scroll>
-            <Scroll html>
-              <Interface />
-            </Scroll>
-          </ScrollControls>
-        </Canvas>
-        <Menu
-          onSectionChange={setSection}
-          menuOpened={menuOpened}
-          setMenuOpened={setMenuOpened}
-        />
-        <Cursor />
-      </MotionConfig>
-      <Leva hidden />
-    </>
-  );
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
