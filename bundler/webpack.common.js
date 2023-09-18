fc845fb2e5dfa7083f8a14b9fbcdfbefb056433d
Yverdon <yverdonb@gmail.com>
2023-09-19 00:30:19 +0200

    first

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/index.js'),
+    entry: path.resolve(__dirname, '../index.js'),
     output:
     {
         filename: 'bundle.[hash].js',
@@ -14,7 +14,7 @@ module.exports = {
     [
         new CopyWebpackPlugin([ { from: path.resolve(__dirname, '../static') } ]),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
+            template: path.resolve(__dirname, '../index.html'),
             minify: true
         })
     ],
