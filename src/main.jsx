7603ae3d3556ba65c1cc23a5cf7d1e3b0a3e6f1d
Yverdon <yverdonb@gmail.com>
2023-12-19 22:49:59 +0100

    yupp

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Canvas } from "@react-three/fiber";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Canvas shadows>
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+  </Canvas>
+);
